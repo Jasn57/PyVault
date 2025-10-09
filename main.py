6338210edad3695df172098b0baac53645b658cb
Jason <jasn74939@gmail.com>
2025-10-09 18:49:43 -0400

    create file struture, and begin writng main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+input = input("1. Login (type 1) 2. Create Account (type 2)")
+
+if input == 1: 
+    input2 = input("1. Add new file (type 1) 2. View files (type 2)")
+    
+    if input2 == 1:
+        newfile == input("Enter new file")
+        # enter new file to be logged on that account
+
+    if input2 == 2:
+        # open files on that account
+
+elif input == 2:
+    username = input("enter username")
+    # when username is enetered ask for password
+
+
+
