93775932d94a26dc8469cf46157bb87ab435b0fd
Jason <jasn74939@gmail.com>
2025-10-10 17:33:00 -0400

    write logic to communicate with json files and restructure json files

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
-input = input("1. Login (type 1) 2. Create Account (type 2)")
+import json
 
-if input == 1: 
-    input2 = input("1. Add new file (type 1) 2. View files (type 2)")
+# main menu
+user_choice = input("1. Login (type 1)  2. Create Account (type 2): ")
+
+if user_choice == "1":  
+    action_choice = input("1. Add new file (type 1)  2. View files (type 2): ")
     
-    if input2 == 1:
-        newfile == input("Enter new file")
-        # enter new file to be logged on that account
+    if action_choice == "1":
+        new_file_name = input("Enter new file: ")
+        
+        # open vault and add new file
+        with open("vault.json", "r") as file:
+            data = json.load(file)
+            new_file_entry = {"new_file": new_file_name}
+            data["vault"].append(new_file_entry)
+        
+        # save updated vault
+        with open("vault.json", "w") as file:
+            json.dump(data, file, indent=4)
 
-    if input2 == 2:
-        # open files on that account
+    elif action_choice == "2":
+        # open and load accounts file
+        with open("accounts.json", "r") as file:
+            data = json.load(file)
 
-elif input == 2:
-    username = input("enter username")
-    # when username is enetered ask for password
+        # print accounts neatly
+        print(json.dumps(data, indent=4))
 
+elif user_choice == "2":
+    # get username and password
+    username = input("Enter username: ")
+    password = input("Enter password: ")
 
+    # load and add new user
+    with open("accounts.json", "r") as file:
+        data = json.load(file)
+        new_user = {"username": username, "password": password}
+        data["accounts"].append(new_user)
 
+    # save to json
+    with open("accounts.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+else:
+    print("Incorrect number. Try again.")
