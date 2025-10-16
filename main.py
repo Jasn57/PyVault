ddce0f4f15b16ef3723377bc505029689602dca2
Jason <jasn74939@gmail.com>
2025-10-16 14:20:47 -0400

    Add encryption for file names in vault
    Integrate encryption for new file names and decrypt when viewing files.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
+from cryptography.fernet import Fernet
 
-# main menu
+key = Fernet.generate_key()
+
 user_choice = input("1. Login (type 1)  2. Create Account (type 2): ")
 
 if user_choice == "1":  
@@ -8,37 +10,31 @@ if user_choice == "1":
     
     if action_choice == "1":
         new_file_name = input("Enter new file: ")
+        new_file_name = encrypted_message = f.encrypt(new_file_name.encode())
         
-        # open vault and add new file
         with open("vault.json", "r") as file:
             data = json.load(file)
             new_file_entry = {"new_file": new_file_name}
             data["vault"].append(new_file_entry)
         
-        # save updated vault
         with open("vault.json", "w") as file:
             json.dump(data, file, indent=4)
 
     elif action_choice == "2":
-        # open and load accounts file
         with open("accounts.json", "r") as file:
             data = json.load(file)
-
-        # print accounts neatly
+        decrypted_message = f.decrypt(encrypted_message).decode()
         print(json.dumps(data, indent=4))
 
 elif user_choice == "2":
-    # get username and password
     username = input("Enter username: ")
     password = input("Enter password: ")
 
-    # load and add new user
     with open("accounts.json", "r") as file:
         data = json.load(file)
         new_user = {"username": username, "password": password}
         data["accounts"].append(new_user)
 
-    # save to json
     with open("accounts.json", "w") as file:
         json.dump(data, file, indent=4)
 
